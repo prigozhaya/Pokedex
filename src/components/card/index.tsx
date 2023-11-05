c38599229d2869987bbdf9fd0ea2e2b244d5e585
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-11-05 04:17:50 +0300

    feat: add elements per page select

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
 import './styles.css';
 import { PokemonCardProps } from '../types/types';
-class PokedexCard extends React.Component<PokemonCardProps> {
-  render() {
-    let number = String(this.props.pokemonsCard.id);
-    if (this.props.pokemonsCard.id < 100) {
+
+export default function PokedexCard(cardProps: PokemonCardProps) {
+
+  let number = String(cardProps.pokemonsCard.id);
+    if (cardProps.pokemonsCard.id < 100) {
       number =
-        this.props.pokemonsCard.id < 10
-          ? '#00' + this.props.pokemonsCard.id
-          : '#0' + this.props.pokemonsCard.id;
+      cardProps.pokemonsCard.id < 10
+          ? '#00' + cardProps.pokemonsCard.id
+          : '#0' + cardProps.pokemonsCard.id;
     }
     return (
       <div className="cardWrapper">
         <img
-          src={this.props.pokemonsCard.img}
+          src={cardProps.pokemonsCard.img}
           alt="pokemon-img"
           className="pokemonImg"
         />
-        <p className="pokemonName">{this.props.pokemonsCard.name}</p>
+        <p className="pokemonName">{cardProps.pokemonsCard.name}</p>
         <p className="pokemonNumber">{number}</p>
         <div className="pokemonInfo">
-          <p className="pokemonType">{this.props.pokemonsCard.types}</p>
+          <p className="pokemonType">{cardProps.pokemonsCard.types}</p>
         </div>
       </div>
     );
   }
-}
-
-export default PokedexCard;
